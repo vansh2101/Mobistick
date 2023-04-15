c88b4354b8d47889a5bdc3ac80b57ca395d00234
Manan Raina Kumar <mananman23@gmail.com>
2023-04-16 01:36:13 +0530

    Update QR generation

--- extension/react/src/pages/Mobile.js
+++ extension/react/src/pages/Mobile.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import QRCode from "react-qr-code";
 
 function Mobile() {
 
-    const token = fetch("http://localhost:8000/token").then(res => res.text()) || "No token"
-    if (token === "No token") {
-        return (
-            <div>
-                <h1>Mobile Login</h1>
-                <p>Can't start server</p>
-            </div>
-        );
-    }
+    // useState to store token
+    const [token, setToken] = useState()
+
+    // fetch token from server, response is sent in res.send() format
+    fetch("http://localhost:8000/token")
+    .then(res => res.text())
+    .then(res => setToken(res))
+
+    // wait for the token to be fetched
+    useEffect(() => {
+        console.log("token fetched")
+        console.log(token)
+    }, [token])
+
     return (
         <div>
             <h1>Mobile Login</h1>
 
             <div style={{height: "auto", margin: "0 auto", maxWidth: 64, width: "100%"}}>
-                <QRCode
+                {token? <QRCode
                     size={256}
                     style={{height: "auto", maxWidth: "100%", width: "100%"}}
                     value={token}
                     viewBox={`0 0 256 256`}
-                />
+                />: <p>Loading...</p> }
             </div>
         </div>
     );
